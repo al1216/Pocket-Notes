9d2a940d33fc16858fc9786812c8488b0bebe371
Aditya Kumar Gupta <adityakumargupta115@gmail.com>
2023-04-28 22:21:59 +0530

    added mobile view and browser view component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import "./App.css";
+import {
+  BrowserView,
+  MobileView,
+} from "react-device-detect";
+import Browser from './BrowserView-Body/Body.jsx'
+import Mobile from './MobileView-Body/Body.jsx'
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserView>
+        <Browser></Browser>
+      </BrowserView>
+      <MobileView>
+        <Mobile></Mobile>
+      </MobileView>
     </div>
   );
 }
